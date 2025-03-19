d6d88980c8529661d9b7907b8a768608f29b7647
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2025-03-19 16:02:46 -0600

    respaldo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsController } from './modules/cars/controllers/cars.controller';
 import { CarsService } from './modules/cars/services/cars.service';
+import { CarsModule } from './modules/cars/cars.module';
+import { BrandsModule } from './modules/brands/brands.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    CarsModule,
+    BrandsModule,
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, CarsController],
   providers: [AppService, CarsService],
 })
--- src/modules/brands/entities/brand.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { BrandEntity } from './brand.entity';
-
-describe('BrandEntity', () => {
-  it('should be defined', () => {
-    expect(new BrandEntity()).toBeDefined();
-  });
-});
